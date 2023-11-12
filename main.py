efbe986745446e8e5fda6b47303ca7376bb0db3c
cyoq <di9akoz@gmail.com>
2023-11-12 20:25:45 +0200

    Add changes to main.py

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ def load_train_data(csv_file: str, sep: str = ";") -> pd.DataFrame:
 
 
 def main():
-    df = load_train_data("./example.csv")
-    class_name = "Play"
-    feature_names = ["Outlook", "Temperature", "Humidity", "Windy"]
+    # df = load_train_data("./example.csv")
+    # class_name = "Play"
+    # feature_names = ["Outlook", "Temperature", "Humidity", "Windy"]
+
+    df = load_train_data("./id3.csv")
+    class_name = "infected_with_X_disease"
+    feature_names = ["fever", "cough", "short_of_breath"]
+
     tree = DecisionTree(df, class_name, feature_names, "Yes", "No")
     tree.train()
     print(tree.root)
+    print(tree.mermaid_diagram_str())
+    tree.create_mermaid_md("new_diagram.md")
 
 
 if __name__ == "__main__":
